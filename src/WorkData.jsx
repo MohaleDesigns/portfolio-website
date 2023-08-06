43d0b0a66c9afb1e222ffae16c4a23a084de7aa6
kabelo <tokolohomohale@gmail.com>
2023-08-06 15:18:19 +0200

    Changing the layout

--- src/WorkData.jsx
+++ src/WorkData.jsx
@@ -1,37 +1,64 @@
-// HTML CSS JS
-import justGym from "../src/assets/work/justgym.png";
-import expenseTracker from "../src/assets/work/expese-tracker.png";
-import wipa from "../src/assets/work/wipa.png";
-
-// WORDEPRESS
-import sanedi from "../src/assets/work/sanedi.png";
-import pacesetter from "../src/assets/work/pacesetter.png";
-
-//REACT
-import movieLand from "../src/assets/work/movieland.png";
-import mohaleBank from "../src/assets/work/mohaleBank.png";
+// WEBSITES
+import justGym from "../src/assets/work/justgym.png"; // HTML CSS JS
+import wipa from "../src/assets/work/wipa.png"; // HTML CSS JS
+import sanedi from "../src/assets/work/sanedi.png"; // WORDEPRESS
+import pacesetter from "../src/assets/work/pacesetter.png"; // WORDEPRESS
+import movieLand from "../src/assets/work/movieland.png"; //REACT
+import mohaleBank from "../src/assets/work/mohaleBank.png"; //REACT
 
 // DESIGN
 import justGymDesign from "../src/assets/work/justGymDesign.png";
 import dynamicdnaDesign from "../src/assets/work/dynamicdnaDesign.png";
+import mohaLix from "../src/assets/work/mohaLix.png";
+
+// PROGRAMMING
+import expenseTracker from "../src/assets/work/expese-tracker.png"; // HTML CSS JS
+import quizzical from "../src/assets/work/quizzical.png"; // REACT
+import freezeDice from "../src/assets/work/freezeDice.png"; // REACT
+
+// DEVELOPEMENT
+import mohaLixDev from "../src/assets/work/mohaLixDev.png"; // REACT
 
 const WorkData = [
   {
     id: 1,
-    image: justGym,
-    title: "Just Gym",
-    category: "HTML - CSS",
+    image: quizzical,
+    title: "Quizzical",
+    category: "PROGRAMMING",
     description:
-      "Just Gym is a front-end development project that I created to showcase my skills. It is a website designed for a gym, with a modern and clean layout that features information about the gym, its facilities, and its services.",
-    tools: "HTML CSS JS BOOTSTRAP",
-    viewDemo: "https://mohale-justgym.netlify.app/",
-    sourceCode: "https://github.com/MohaleDesigns/Just-Gym",
+      "Quizzical is an engaging web app that I developed for personal exploration. It allows users to test their knowledge about me through a series of quizzes. Users are presented with a comprehension before answering questions related to my background, interests, and experiences.",
+    tools: "REACT",
+    viewDemo: "https://gamequizzical.netlify.app",
+    sourceCode: "https://github.com/MohaleDesigns/quiz-version-two",
   },
   {
     id: 2,
+    image: freezeDice,
+    title: "Freeze Dice",
+    category: "PROGRAMMING",
+    description:
+      "In this interactive game, users roll virtual dice with the goal of getting all the dice to display the same number. The twist is that users can strategically 'freeze' the dice at their current values by clicking on them. This project demonstrates my creative approach to web app development and offers users an entertaining and challenging experience.",
+    tools: "REACT",
+    viewDemo: "https://dicefreeze.netlify.app/",
+    sourceCode: "https://github.com/MohaleDesigns/dice-freeze",
+  },
+  {
+    id: 3,
+    image: mohaLix,
+    title: "mohaLix",
+    category: "DESIGN",
+    description:
+      "MohaLix is a personal project where I delved into web development by creating a clone of the popular streaming platform, Netflix. Through this project, I honed my skills in designing and structuring a complex web application. MohaLix allows users to browse and watch a collection of videos, bringing together my passion for coding and content consumption in one project.",
+    tools: "Figma",
+    viewDemo:
+      "https://www.figma.com/file/ADlgStZUH2VRYuoDme6ykm/Web-Designs?type=design&node-id=0%3A1&mode=design&t=295uUBu2kDucrZrK-1",
+    sourceCode: "none",
+  },
+  {
+    id: 4,
     image: expenseTracker,
     title: "Expense Tracker",
-    category: "HTML - CSS",
+    category: "PROGRAMMING",
     description:
       "Expense Tracker is a front-end development project that I created to demonstrate my skills in web development. The app allows users to track their expenses and income, with features such as adding new transactions, categorizing them, and generating reports.",
     tools: "HTML CSS JS",
@@ -39,10 +66,21 @@ const WorkData = [
     sourceCode: "https://github.com/MohaleDesigns/Expense-Tracker",
   },
   {
-    id: 3,
+    id: 5,
+    image: justGym,
+    title: "Just Gym",
+    category: "WEBSITE",
+    description:
+      "Just Gym is a front-end development project that I created to showcase my skills. It is a website designed for a gym, with a modern and clean layout that features information about the gym, its facilities, and its services.",
+    tools: "HTML CSS JS BOOTSTRAP",
+    viewDemo: "https://mohale-justgym.netlify.app/",
+    sourceCode: "https://github.com/MohaleDesigns/Just-Gym",
+  },
+  {
+    id: 6,
     image: sanedi,
     title: "SANEDI",
-    category: "WORDPRESS",
+    category: "WEBSITE",
     description:
       "This project was done in collaboration with a team at Ditheto Group, and the client was SANEDI, a government company that deals with energy. The website was designed to provide information about SANEDI's mission, goals, and activities, with a focus on sustainability and renewable energy.",
     tools: "WORDPRESS",
@@ -50,21 +88,32 @@ const WorkData = [
     sourceCode: "none",
   },
   {
-    id: 4,
+    id: 7,
     image: pacesetter,
     title: "Pacesetter",
-    category: "WORDPRESS",
+    category: "WEBSITE",
     description:
       "Pacesetter is a website that I created for a consulting firm that provides business solutions to various industries. It was a challenging project that I completed all by myself, and it involved designing a website that would reflect the professionalism and expertise of the company, while also being user-friendly and informative.",
     tools: "WORDPRESS",
-    viewDemo: "https://pacesetter.dithetogroup.co.za/",
+    viewDemo: "https://www.digitalpacesetter.com/",
     sourceCode: "none",
   },
   {
-    id: 5,
+    id: 8,
+    image: mohaLixDev,
+    title: "MohaLix",
+    category: "IN PROGRESS",
+    description:
+      "MohaLix is a personal project where I delved into web development by creating a clone of the popular streaming platform, Netflix. Through this project, I honed my skills in designing and structuring a complex web application. MohaLix will allows users to browse and watch a collection of videos, bringing together my passion for coding and content consumption in one project.",
+    tools: "REACT",
+    viewDemo: "none",
+    sourceCode: "none",
+  },
+  {
+    id: 9,
     image: movieLand,
     title: "Movie Land",
-    category: "REACT",
+    category: "PROGRAMMING",
     description:
       "The Movie Land is a React web app that I created as a personal project. It fetches movie data from an API and allows users to search for movies, view their details, and save them to a favorites list. The app features a responsive design and uses modern web technologies to provide a seamless user experience.",
     tools: "REACT",
@@ -72,10 +121,10 @@ const WorkData = [
     sourceCode: "https://github.com/MohaleDesigns/MovieLand",
   },
   {
-    id: 6,
+    id: 10,
     image: wipa,
     title: "Women In Power",
-    category: "HTML - CSS",
+    category: "WEBSITE",
     description:
       "Women In Power is a client project that involved developing a website, the website serves as a platform to advertise past and upcoming events organized by the company. Users can explore event details, including information about the speakers and the agenda, with a focus on showcasing the empowerment and achievements of women.",
     tools: "HTML CSS JS BOOTSTRAP",
@@ -83,7 +132,7 @@ const WorkData = [
     sourceCode: "https://github.com/MohaleDesigns/WomenInPower",
   },
   {
-    id: 7,
+    id: 11,
     image: justGymDesign,
     title: "Just Gym",
     category: "DESIGN",
@@ -95,7 +144,7 @@ const WorkData = [
     sourceCode: "none",
   },
   {
-    id: 8,
+    id: 12,
     image: dynamicdnaDesign,
     title: "Dynamic DNA Revamp",
     category: "DESIGN",
@@ -107,10 +156,10 @@ const WorkData = [
     sourceCode: "none",
   },
   {
-    id: 9,
+    id: 13,
     image: mohaleBank,
     title: "Mohale Bank",
-    category: "REACT",
+    category: "WEBSITE",
     description:
       "Mohale Bank is a React website that I created to sharpen my skills in React development. It is a fictional modern bank website that features a sleek and user-friendly interface, with sections for banking services, online banking, loans, and more. The website is designed to be responsive and accessible on all devices, and it incorporates modern web technologies to deliver a seamless user experience.",
     tools: "REACT",
