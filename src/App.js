eebe10a687346b42589d4379ec6066ef413d85d7
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2024-11-11 20:48:13 +0200

    fix: removed unused components & code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-import { useState } from "react";
 import "./App.css";
 
 import Hero from "./components/Hero";
-// import About from "./components/About";
-import Tools from "./components/Tools";
 import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import WorkButtons from "./components/WorkButtons";
-import WorkFiltered from "./components/WorkFiltered";
 
-import items from "./WorkData";
+import StackSlider from "./components/StackSlider";
+import ProjectsSection from "./components/ProjectsSection";
+import CallToAction from "./components/CallToAction";
 
 function App() {
-  const AllCategory = ["All", ...new Set(items.map((item) => item.category))];
-
-  const [menuItems, setMenuItems] = useState(items);
-  const [buttons] = useState(AllCategory);
-
-  const filter = (buttons) => {
-    if (buttons === "All") {
-      setMenuItems(items);
-      return;
-    }
-
-    const filteredData = items.filter((item) => item.category === buttons);
-    setMenuItems(filteredData);
-  };
-
   return (
-    <div className="App overflow-hidden">
+    <div className="App">
       <Header />
       <Hero />
-      <Nav />
-      {/* <About /> */}
-      <Tools />
-      <WorkButtons buttons={buttons} filter={filter} />
-      <WorkFiltered menuItems={menuItems} />
-      <Contact />
+      <StackSlider />
+      <ProjectsSection />
+      <CallToAction />
       <Footer />
     </div>
   );
