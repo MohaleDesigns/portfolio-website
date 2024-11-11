1feaaacf4ba71001aaa6380a30ad24f804c40189
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2024-11-11 20:58:43 +0200

    fix: updated projects

--- src/WorkData.jsx
+++ src/WorkData.jsx
@@ -1,30 +1,33 @@
 // WEBSITES
+import pacesetter from "../src/assets/work/pacesetter.png"; // WORDEPRESS
+import sanedi from "../src/assets/work/sanedi.png"; // WORDEPRESS
+
 import justGym from "../src/assets/work/justgym.png"; // HTML CSS JS
 import wipa from "../src/assets/work/wipa.png"; // HTML CSS JS
-import sanedi from "../src/assets/work/sanedi.png"; // WORDEPRESS
-import pacesetter from "../src/assets/work/pacesetter.png"; // WORDEPRESS
-import movieLand from "../src/assets/work/movieland.png"; //REACT
-import mohaleBank from "../src/assets/work/mohaleBank.png"; //REACT
+import expenseTracker from "../src/assets/work/expese-tracker.png"; // HTML CSS JS
 
-// DESIGN
+// DESIGNS
 import justGymDesign from "../src/assets/work/justGymDesign.png";
 import dynamicdnaDesign from "../src/assets/work/dynamicdnaDesign.png";
 import mohaLix from "../src/assets/work/mohaLix.png";
 
-// PROGRAMMING
-import expenseTracker from "../src/assets/work/expese-tracker.png"; // HTML CSS JS
+// WEB APPS
+import movieLand from "../src/assets/work/movieland.png"; //REACT
+import mohaleBank from "../src/assets/work/mohaleBank.png"; //REACT
 import quizzical from "../src/assets/work/quizzical.png"; // REACT
 import freezeDice from "../src/assets/work/freezeDice.png"; // REACT
-
-// DEVELOPEMENT
 import mohaLixDev from "../src/assets/work/mohaLixDev.png"; // REACT
+
+// MOBILE APPS
+
+// IN PROGRESS
 
 const WorkData = [
   {
     id: 1,
     image: quizzical,
-    title: "Quizzical",
-    category: "PROGRAMMING",
+    projectName: "Quizzical",
+    projectType: "webApps",
     description:
       "Quizzical is an engaging web app that I developed for personal exploration. It allows users to test their knowledge about me through a series of quizzes. Users are presented with a comprehension before answering questions related to my background, interests, and experiences.",
     tools: "REACT Tailwind-CSS",
@@ -34,8 +37,8 @@ const WorkData = [
   {
     id: 2,
     image: freezeDice,
-    title: "Freeze Dice",
-    category: "PROGRAMMING",
+    projectName: "Freeze Dice",
+    projectType: "webApps",
     description:
       "In this interactive game, users roll virtual dice with the goal of getting all the dice to display the same number. The twist is that users can strategically 'freeze' the dice at their current values by clicking on them. This project demonstrates my creative approach to web app development and offers users an entertaining and challenging experience.",
     tools: "REACT Tailwind-CSS",
@@ -45,8 +48,8 @@ const WorkData = [
   {
     id: 3,
     image: mohaLix,
-    title: "mohaLix",
-    category: "DESIGN",
+    projectName: "mohaLix",
+    projectType: "designs",
     description:
       "MohaLix is a personal project where I delved into web development by creating a clone of the popular streaming platform, Netflix. Through this project, I honed my skills in designing and structuring a complex web application. MohaLix allows users to browse and watch a collection of videos, bringing together my passion for coding and content consumption in one project.",
     tools: "Figma",
@@ -57,8 +60,8 @@ const WorkData = [
   {
     id: 4,
     image: expenseTracker,
-    title: "Expense Tracker",
-    category: "PROGRAMMING",
+    projectName: "Expense Tracker",
+    projectType: "webApps",
     description:
       "Expense Tracker is a front-end development project that I created to demonstrate my skills in web development. The app allows users to track their expenses and income, with features such as adding new transactions, categorizing them, and generating reports.",
     tools: "HTML CSS JS",
@@ -68,8 +71,8 @@ const WorkData = [
   {
     id: 5,
     image: justGym,
-    title: "Just Gym",
-    category: "WEBSITE",
+    projectName: "Just Gym",
+    projectType: "websites",
     description:
       "Just Gym is a front-end development project that I created to showcase my skills. It is a website designed for a gym, with a modern and clean layout that features information about the gym, its facilities, and its services.",
     tools: "HTML CSS JS BOOTSTRAP",
@@ -79,8 +82,8 @@ const WorkData = [
   {
     id: 6,
     image: sanedi,
-    title: "SANEDI",
-    category: "WEBSITE",
+    projectName: "SANEDI",
+    projectType: "websites",
     description:
       "This project was done in collaboration with a team at Ditheto Group, and the client was SANEDI, a government company that deals with energy. The website was designed to provide information about SANEDI's mission, goals, and activities, with a focus on sustainability and renewable energy.",
     tools: "WORDPRESS",
@@ -90,8 +93,8 @@ const WorkData = [
   {
     id: 7,
     image: pacesetter,
-    title: "Pacesetter",
-    category: "WEBSITE",
+    projectName: "Pacesetter",
+    projectType: "websites",
     description:
       "Pacesetter is a website that I created for a consulting firm that provides business solutions to various industries. It was a challenging project that I completed all by myself, and it involved designing a website that would reflect the professionalism and expertise of the company, while also being user-friendly and informative.",
     tools: "WORDPRESS",
@@ -101,8 +104,8 @@ const WorkData = [
   {
     id: 8,
     image: mohaLixDev,
-    title: "MohaLix",
-    category: "IN PROGRESS",
+    projectName: "MohaLix",
+    projectType: "designs",
     description:
       "MohaLix is a personal project where I delved into web development by creating a clone of the popular streaming platform, Netflix. Through this project, I honed my skills in designing and structuring a complex web application. MohaLix will allows users to browse and watch a collection of videos, bringing together my passion for coding and content consumption in one project.",
     tools: "REACT Tailwind-CSS",
@@ -112,8 +115,8 @@ const WorkData = [
   {
     id: 9,
     image: movieLand,
-    title: "Movie Land",
-    category: "PROGRAMMING",
+    projectName: "Movie Land",
+    projectType: "webApps",
     description:
       "The Movie Land is a React web app that I created as a personal project. It fetches movie data from an API and allows users to search for movies, view their details, and save them to a favorites list. The app features a responsive design and uses modern web technologies to provide a seamless user experience.",
     tools: "REACT",
@@ -123,8 +126,8 @@ const WorkData = [
   {
     id: 10,
     image: wipa,
-    title: "Women In Power",
-    category: "WEBSITE",
+    projectName: "Women In Power",
+    projectType: "websites",
     description:
       "Women In Power is a client project that involved developing a website, the website serves as a platform to advertise past and upcoming events organized by the company. Users can explore event details, including information about the speakers and the agenda, with a focus on showcasing the empowerment and achievements of women.",
     tools: "HTML CSS JS BOOTSTRAP",
@@ -134,8 +137,8 @@ const WorkData = [
   {
     id: 11,
     image: justGymDesign,
-    title: "Just Gym",
-    category: "DESIGN",
+    projectName: "Just Gym",
+    projectType: "designs",
     description:
       "Just Gym is a design project that I created using Figma. It is a web design concept for a gym, where I focused on creating an engaging and visually appealing interface. The design showcases various sections of the gym website, including information about services, classes, trainers, and membership details. Through this project, I aimed to demonstrate my proficiency in designing intuitive and aesthetically pleasing user interfaces for gym websites.",
     tools: "FIGMA",
@@ -146,8 +149,8 @@ const WorkData = [
   {
     id: 12,
     image: dynamicdnaDesign,
-    title: "Dynamic DNA Revamp",
-    category: "DESIGN",
+    projectName: "Dynamic DNA Revamp",
+    projectType: "designs",
     description:
       "Dynamic DNA Revamp is a design project where I showcased my web design skills. Using Figma, I created a modern and responsive design for both desktop and mobile platforms. The revamped landing page showcases a visually appealing layout, intuitive navigation, and effective use of typography and color schemes. The project allowed me to demonstrate my expertise in designing user-centric and visually appealing interfaces for websites.",
     tools: "FIGMA",
@@ -158,8 +161,8 @@ const WorkData = [
   {
     id: 13,
     image: mohaleBank,
-    title: "Mohale Bank",
-    category: "WEBSITE",
+    projectName: "Mohale Bank",
+    projectType: "websites",
     description:
       "Mohale Bank is a React website that I created to sharpen my skills in React development. It is a fictional modern bank website that features a sleek and user-friendly interface, with sections for banking services, online banking, loans, and more. The website is designed to be responsive and accessible on all devices, and it incorporates modern web technologies to deliver a seamless user experience.",
     tools: "REACT",
